30d222afd4050d0cc9f45f5c323d3b19c524668c
TiagoMuller <tiagomuller5100@gmail.com>
2023-06-01 11:05:07 -0300

    Studies on HashSet, LinkedHashSet, TreeSet and other exercises about!

---
+++ Exercice_Set/src/application/Program.java
@@ -0,0 +1,37 @@
+package application;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Program {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		Set<Integer> cursoA = new HashSet<>();
+		Set<Integer> cursoB = new HashSet<>();
+		Set<Integer> cursoC = new HashSet<>();
+		
+		System.out.print("How many students for course A: ");
+		int n1 = sc.nextInt();
+		for(int i = 0; i < n1; i++) {
+			cursoA.add(sc.nextInt());
+		}
+		
+		System.out.print("How many students for course B: ");
+		int n2 = sc.nextInt();
+		for(int i = 0; i < n2; i++) {
+			cursoA.add(sc.nextInt());
+		}
+		
+		System.out.print("How many students for course C: ");
+		int n3 = sc.nextInt();
+		for(int i = 0; i < n3; i++) {
+			cursoA.add(sc.nextInt());
+		}
+		
+		System.out.println("Total students: " +cursoA.size());
+	}
+}
