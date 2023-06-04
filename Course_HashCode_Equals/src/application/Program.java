325277c2f0588ff0b13e4b5cfb140ea7c26a3fe9
Tiago Müller <tiagomuller5100@gmail.com>
2023-06-04 18:38:19 -0300

    Update Program.java

--- Course_HashCode_Equals/src/application/Program.java
+++ Course_HashCode_Equals/src/application/Program.java
@@ -7,7 +7,7 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Client c1 = new Client("Maria", "maria@gmail.com");
-		Client c2 = new Client("Maria", "maria@gmail.com");
+		Client c2 = new Client("Alex", "alex@gmail.com");
 		
 		String s1 = "Test";
 		String s2 = "Test";
